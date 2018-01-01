f86f9aca8c570dff7f37bae6dadfacb21b96b470
Craig Stewart <craig@ardlodge.co.uk>
2018-01-01 15:58:11 +0000

    delete

--- javascript/button.js
+++
@@ -1,3 +0,0 @@
-var buttonLines = '<span class="button-lines"> <div class="top"></div><div class="right"></div> <div class="bottom"></div><div class="left"></div></span>';
-
-$(".button").append(buttonLines);
